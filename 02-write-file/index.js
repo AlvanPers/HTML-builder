e9a3a3f30d3d346beb2a8be9c105bd6edbd27cc5
Alex <alexyoup@outlook.com>
2024-01-22 18:25:01 +0300

    refactor: add comments

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,23 +6,23 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Привет! Введите текст:');
+console.log('Hello! Enter text:');
 
 rl.on('line', (input) => {
   if (input === 'exit') {
-    console.log('До свидания!');
+    console.log('Goodbye!');
     process.exit(0);
   } else {
     fs.appendFile('02-write-file/02-write-file.txt', `${input}\n`, (err) => {
       if (err) throw err;
       console.log(
-        'Текст был добавлен в файл. Введите еще текст или введите "exit" для выхода или намите ctrl+c.',
+        'The text has been added to the file. Enter more text or enter "exit" to exit or press ctrl+c.',
       );
     });
   }
 });
 
 process.on('SIGINT', () => {
-  console.log('\nДо свидания!');
+  console.log('\nGoodbye!');
   process.exit(0);
 });
