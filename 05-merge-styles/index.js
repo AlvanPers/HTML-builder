f8bdd92d4490bcc9c7062e083744b88639a94faa
Alex <alexyoup@outlook.com>
2024-01-25 19:31:32 +0300

    fix: corrected errors 5 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,22 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 function mergeStyles(src, dest) {
-  // Создаем папку назначения, если она еще не существует
   if (!fs.existsSync(dest)) {
     fs.mkdirSync(dest, { recursive: true });
   }
-
-  // Читаем содержимое исходной папки
   const entries = fs.readdirSync(src, { withFileTypes: true });
 
-  // Создаем поток записи для файла bundle.css
   const writeStream = fs.createWriteStream(path.join(dest, 'bundle.css'));
 
   for (let entry of entries) {
     const srcPath = path.join(src, entry.name);
 
     if (entry.isFile() && path.extname(srcPath) === '.css') {
-      // Если элемент является CSS-файлом, читаем его и записываем в bundle.css
       const data = fs.readFileSync(srcPath, 'utf8');
       writeStream.write(data);
     }
