bc9df20c68100918e0f6f71ab6c3b2cbb41c9268
Alex <alexyoup@outlook.com>
2024-01-25 20:16:41 +0300

    fix: corrected errors - Sync

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -27,14 +27,3 @@ fs.readdir(directoryPath, function (err, files) {
     });
   });
 });
-
-//        другой способ
-// const fs = require("fs");
-// const path = require("path");
-// const absoluteFrom = path.resolve(__dirname, "files");
-// const absoluteTo = path.resolve(__dirname, "files-copy");
-// const files = fs.readdirSync(absoluteFrom);
-
-// for (const file of files) {
-//     fs.copyFile(`${absoluteFrom}/${file}`, `${absoluteTo}/${file}`, (err) => {console.log(err)});
-// }
--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,41 +1,29 @@
-// const fs = require('fs');
-// const path = require('path');
-
-// function mergeStyles(src, dest) {
-//   if (!fs.existsSync(dest)) {
-//     fs.mkdirSync(dest, { recursive: true });
-//   }
-//   const entries = fs.readdirSync(src, { withFileTypes: true });
-
-//   const writeStream = fs.createWriteStream(path.join(dest, 'bundle.css'));
-
-//   for (let entry of entries) {
-//     const srcPath = path.join(src, entry.name);
-
-//     if (entry.isFile() && path.extname(srcPath) === '.css') {
-//       const data = fs.readFileSync(srcPath, 'utf8');
-//       writeStream.write(data);
-//     }
-//   }
-
-//   writeStream.end();
-// }
-
-// mergeStyles('./05-merge-styles/styles', './05-merge-styles/project-dist');
-
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
-const absoluteFrom = path.resolve(__dirname, 'styles');
-const absoluteTo = path.resolve(__dirname, 'project-dist');
-const files = fs.readdirSync(absoluteFrom);
-const arr = [];
-for (const file of files) {
-  const fileConf = fs.statSync(`${absoluteFrom}/${file}`);
-  const extN = path.extname(`${absoluteFrom}/${file}`);
-  if (fileConf.isFile() && extN === '.css') {
-    const fileContent = fs.readFileSync(`${absoluteFrom}/${file}`, 'utf-8');
-    arr.push(fileContent);
+
+async function mergeStyles(src, dest) {
+  try {
+    await fs.access(dest);
+  } catch {
+    await fs.mkdir(dest, { recursive: true });
   }
+
+  const entries = await fs.readdir(src, { withFileTypes: true });
+
+  const writeStream = require('fs').createWriteStream(
+    path.join(dest, 'bundle.css'),
+  );
+
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+
+    if (entry.isFile() && path.extname(srcPath) === '.css') {
+      const data = await fs.readFile(srcPath, 'utf8');
+      writeStream.write(data);
+    }
+  }
+
+  writeStream.end();
 }
-const bundleContent = arr.join('');
-fs.writeFileSync(`${absoluteTo}/bundle.css`, bundleContent);
+
+mergeStyles('./05-merge-styles/styles', './05-merge-styles/project-dist');
